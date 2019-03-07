5bf605db6fa0606a69eb4daff9c7018ac528de0c
nawazahmad20 <nawazahmad20@numbertheory.ai>
2019-03-07 23:33:21 +0530

    changed for importing in another file

--- motor_control.py
+++ motor_control.py
@@ -1,8 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM) # set the mode to broadcomm
-GPIO.cleanup()
 
 #choose the GPIO pins
 fwd1 = 23  # pin 16
@@ -10,11 +8,6 @@ bwd1 = 24 #pin 18
 fwd2 = 16 # pin 36
 bwd2 = 20 # pin 38
 
-# declare selected pin as output pin
-GPIO.setup(fwd1, GPIO.OUT)
-GPIO.setup(bwd1, GPIO.OUT)
-GPIO.setup(fwd2, GPIO.OUT)
-GPIO.setup(bwd2, GPIO.OUT)
 
 sleep_time = .2
 
@@ -49,4 +42,15 @@ def stop():
   GPIO.output(fwd2, GPIO.LOW)
   GPIO.output(bwd2, GPIO.LOW)
 
+
+if __name__ == "__main__":
+  GPIO.setmode(GPIO.BCM) # set the mode to broadcomm
+  GPIO.cleanup()
+
+
+  # declare selected pin as output pin
+  GPIO.setup(fwd1, GPIO.OUT)
+  GPIO.setup(bwd1, GPIO.OUT)
+  GPIO.setup(fwd2, GPIO.OUT)
+  GPIO.setup(bwd2, GPIO.OUT)
 
