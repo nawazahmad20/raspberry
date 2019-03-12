ceac9ece6c6d0008764a44d5cc7fa0028db140ae
nawazahmad20 <nawazahmad20@numbertheory.ai>
2019-03-13 00:09:49 +0530

    added line follower training and main

---
+++ lineFollower.py
@@ -0,0 +1,83 @@
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+import time
+import RPi.GPIO as GPIO
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import motor_control as mc
+
+GPIO.setmode(GPIO.BCM)
+
+
+#choose the GPIO pins for mptor control
+fwd1 = 23  # pin 16
+bwd1 = 24 #pin 18
+fwd2 = 16 # pin 36
+bwd2 = 20 # pin 38
+
+# declare selected pin as output pin
+GPIO.setup(fwd1, GPIO.OUT)
+GPIO.setup(bwd1, GPIO.OUT)
+GPIO.setup(fwd2, GPIO.OUT)
+GPIO.setup(bwd2, GPIO.OUT)
+
+model = load_model("model")
+
+
+def control_robot(image):
+    prediction = np.argmax(model.predict(image))
+    if prediction == 0:
+        print("forward")
+        mc.forward()
+    elif prediction == 1:
+        print("left")
+        mc.left()
+    else:
+        print("right")
+        mc.right()
+
+
+if __name__ == "__main__":
+    try:
+        # initialize the camera and grab a reference to the raw camera capture
+        camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 32
+        rawCapture = PiRGBArray(camera, size=(640, 480))
+
+        # allow the camera to warmup
+        time.sleep(0.1)
+
+        # capture frames from the camera
+        start = 1
+
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+          # grab the raw NumPy array representing the image, then initialize the timestamp
+          # and occupied/unoccupied text
+          image = frame.array
+          # show the frame
+          cv2.imshow("Frame", image)
+          key = cv2.waitKey(1) & 0xFF
+          #cv2.imwrite(str(start) + ".jpg", image)
+          #start = start + 1
+
+          control_robot(image)
+
+          # clear the stream in preparation for the next frame
+          rawCapture.truncate(0)
+
+          # if the `q` key was pressed, break from the loop
+          if key == ord("q"):
+            break
+          time.sleep(.2)
+
+    except KeyboardInterrupt:
+        mc.stop()
+        GPIO.cleanup()
+        sys.exit()
---
+++ trainCNN.py
@@ -0,0 +1,139 @@
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+# set the matplotlib backend so figures can be saved in the background
+#import matplotlib
+#matplotlib.use("Agg")
+ 
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+#from pyimagesearch.lenet import LeNet
+from imutils import paths
+import numpy as np
+import argparse
+import random
+import cv2
+import os
+
+
+
+class LeNet:
+  @staticmethod
+  def build(width, height, depth, classes):
+    # initialize the model
+    model = Sequential()
+    inputShape = (height, width, depth)
+
+    # if we are using "channels first", update the input shape
+    if K.image_data_format() == "channels_first":
+      inputShape = (depth, height, width)
+
+    # first set of CONV => RELU => POOL layers
+    model.add(Conv2D(20, (5, 5), padding="same",
+      input_shape=inputShape))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # second set of CONV => RELU => POOL layers
+    model.add(Conv2D(50, (5, 5), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # first (and only) set of FC => RELU layers
+    model.add(Flatten())
+    model.add(Dense(500))
+    model.add(Activation("relu"))
+
+    # softmax classifier
+    model.add(Dense(classes))
+    model.add(Activation("softmax"))
+
+    # return the constructed network architecture
+    return model 
+
+
+
+dataset = '/home/pi/Desktop/tutorials/raspberry'
+# initialize the number of epochs to train for, initial learning rate,
+# and batch size
+EPOCHS = 5
+INIT_LR = 1e-3
+BS = 32
+ 
+# initialize the data and labels
+print("[INFO] loading images...")
+data = []
+labels = []
+ 
+# grab the image paths and randomly shuffle them
+imagePaths = sorted(list(paths.list_images(dataset)))
+random.seed(42)
+random.shuffle(imagePaths)
+
+# loop over the input images
+for imagePath in imagePaths:
+    # load the image, pre-process it, and store it in the data list
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (28, 28))
+    image = img_to_array(image)
+    data.append(image)
+
+    # extract the class label from the image path and update the
+    # labels list
+    label = imagePath.split(os.path.sep)[-2]
+    if label == 'forward':
+        label = 0
+    elif label == 'right':
+        label = 1
+    else:
+        label =2
+    labels.append(label)
+
+# scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+ 
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+    labels, test_size=0.25, random_state=42)
+ 
+
+#trainX = data[:-50]
+#testX = data[-50:]
+#trainY = labels[:-50]
+#testY = labels[-50:]
+
+
+# convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes=3)
+testY = to_categorical(testY, num_classes=3)
+
+
+
+# initialize the model
+print("[INFO] compiling model...")
+model = LeNet.build(width=28, height=28, depth=3, classes=3)
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+    metrics=["accuracy"])
+ 
+# train the network
+print("[INFO] training network...")
+H = model.fit(trainX, trainY, batch_size=BS,
+    validation_data=(testX, testY),# steps_per_epoch=len(trainX) // BS,
+    epochs=EPOCHS, verbose=1)
+ 
+# save the model to disk
+print("[INFO] serializing network...")
+model.save("model")
+
