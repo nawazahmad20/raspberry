491b09e8ebcf2d21e26c53d557f8419c7b0697cf
nawazahmad20 <nawazahmad20@numbertheory.ai>
2019-05-07 00:06:12 +0530

    cleaned code of surveillanceRobot.py

--- surveillanceRobot.py
+++ surveillanceRobot.py
@@ -4,27 +4,16 @@ from multiprocessing import Pool
 import time
 import RPi.GPIO as GPIO
 import Utils.motor_control as mc
-import Utils.keydetection as kd
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 import sys
 import readchar
-
 import curses
-import os
 
 GPIO.setmode(GPIO.BCM)
 
-# define the GPIO pins for trigger and echo
-TRIG = 17 # pin 11
-ECHO = 22 # pin 15
-
-# define the pins as input/output
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-
-#choose the GPIO pins for mptor control
+#choose the GPIO pins for motor control
 fwd1 = 23  # pin 16
 bwd1 = 24  # pin 18
 fwd2 = 16  # pin 36
@@ -36,12 +25,8 @@ GPIO.setup(bwd1, GPIO.OUT)
 GPIO.setup(fwd2, GPIO.OUT)
 GPIO.setup(bwd2, GPIO.OUT)
 
-key_global = None
-
+# function to control direction of motors/robot based on pressed key
 def movementOnKeyPress(key):
-  #global key_global
-  #key =  key_global
-  print(key)
   if key == 'w':
     mc.forward()
   elif key == 's':
@@ -53,10 +38,9 @@ def movementOnKeyPress(key):
   elif key == " ":
     mc.stop()
   else:
-  #time.sleep(.1)
     mc.stop()
 
-
+# function to display images from raspberry pi camera
 def cameradisplay():
   # initialize the camera and grab a reference to the raw camera capture
   camera = PiCamera()
@@ -65,7 +49,6 @@ def cameradisplay():
   rawCapture = PiRGBArray(camera, size=(640, 480))
   # allow the camera to warmup
   time.sleep(0.5)
-
   for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
@@ -73,37 +56,30 @@ def cameradisplay():
     # show the frame
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
-    #image = cv2.resize(image, (28, 28))
-    #image = img_to_array(image)
-    #image = np.array(image, dtype="float") / 255.0
-    #image = image.reshape(-1, 28, 28, 3)
-    #cv2.imshow("Frame", image[0])
-    #movementOnKeyPress(key)
     rawCapture.truncate(0)
-    #time.sleep(.5)
 
 
+# function to get the key pressed on keyboard
 def main(win):
-    global key_global
-    win.nodelay(True)
-    key=""
-    win.clear()
-    win.addstr("Detected key:")
-    while 1:
-        try:
-           key = win.getkey()
-           win.clear()
-           win.addstr("Detected key:")
-           win.addstr(str(key))
-           movementOnKeyPress(str(key))
-           #key_global = key
-           if key == os.linesep:
-              break
-        except Exception as e:
-           # No input
-           pass
+  global key_global
+  win.nodelay(True)
+  key=""
+  win.clear()
+  win.addstr("Detected key:")
+  while 1:
+    try:
+      key = win.getkey()
+      win.clear()
+      win.addstr("Detected key:")
+      win.addstr(str(key))
+      movementOnKeyPress(str(key)) # move the robot based on input key
+      if key == os.linesep:
+        break
+    except Exception as e:
+      #No input
+      pass
 
-
+# function to run processes in multiprocessing
 def run_process(process):
   try:
     process()
@@ -113,11 +89,10 @@ def run_process(process):
 
 if __name__ == "__main__":
   try:
-    processes = (cameradisplay, main,  movementOnKeyPress)
+    processes = (cameradisplay, main)
     pool = Pool(processes=2)
-    pool.map(run_process, processes)
-
+    pool.map(run_process, processes) # run both the processes in parallel
   except KeyboardInterrupt:
-    mc.stop()
+    mc.stop() # stop rotation of motors during termination of script
     GPIO.cleanup()
     sys.exit()
