bfe5129be1eace3aea7d088121a427cd8c1d6ac7
nawazahmad20 <nawazahmad20@numbertheory.ai>
2020-10-12 11:11:19 +0530

    deleted files that were moved

--- camera-example.py
+++
@@ -1,9 +0,0 @@
-import picamera
-import time
-
-camera = picamera.PiCamera()
-camera.vflip = True
-#camera.capture('test.jpg')
-camera.start_recording('example_vid.h264')
-time.sleep(10)
-camera.stop_recording()
